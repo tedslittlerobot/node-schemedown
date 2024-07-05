67edb24c0b0d39d6a6bd00c3a9847f284e5f48e1
Stefan Horner <stefan.horner@umotif.com>
2024-07-05 22:39:49 +0100

    refactor utils

--- src/utils/io.ts
+++ src/utils/io.ts
@@ -1,28 +1,5 @@
-import {readFile, readdir} from 'node:fs/promises';
 import {type Schema} from '../types.js';
-
-export async function getJsonFiles(source: string): Promise<string[]> {
-	const files = await readdir(source, {recursive: true});
-	return files
-		.filter(item => item.endsWith('.json'))
-		.map(item => `${source}/${item}`);
-}
-
-export async function loadJson(source: string): Promise<Schema> {
-	const contents = await readFile(source, 'utf8');
-
-	if (!contents) {
-		throw new Error(`No such file: ${source}`);
-	}
-
-	const json = JSON.parse(contents) as Schema;
-
-	if (!json) {
-		throw new Error(`Could not create JSON from file ${source}`);
-	}
-
-	return json;
-}
+import {getJsonFiles, loadJson} from './json.js';
 
 export async function getSchemasFromDirectory(source: string): Promise<Record<string, Schema>> {
 	const files = await getJsonFiles(source);
